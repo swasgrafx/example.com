6c5bd9ed43fbd424d443b244eab550156c7fc88a
CHAD-SVASTISALEE <SWASGRAFX@GMAIL.COM>
2018-08-31 14:00:08 -0500

    initial php commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello, Welcome to Chad Svastisalee's Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+<body>
+<header>
+      <span class="logo">My Website</span>
+      <a id="toggleMenu">Menu</a>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="resume.php">Resume</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+<h1>Hello</h1>
+<p>Welcome, This is Chad Svastisalee</p>
+<img style="border-radius: 50%; float: right; margin-right: 1em;" src="https://s.gravatar.com/avatar/ccb8db9b92f24f1bf1dc575a85c78feb?s=80" alt="Chad-Svastisalee">
+<!-- Gravator Image Box
+    <img src="https://s.gravatar.com/avatar/ccb8db9b92f24f1bf1dc575a85c78feb?s=80" alt="Chad-Svastisalee">
+-->
+<h2>HTML Elements</h2>
+    <p>
+    To paraphrase the lesson text [...] For example &lt;h1&gt;h1&lt;h2&gt;title&lt;/h1&gt;... Notice the use of character entities when wanting show the tags in HTML.
+    </p>
+    <h2>Character Entities</h2>
+    <p>Since the keyboard does not have a &copy; key we need a way to reference this so we say &amp;copy;. Additionally, greater than and less than are interpreted as HTML tags. These are examples of symbols that we may want to display but will not be able to with out a work around. This is where character entities come into play. </p>
+    <ul>
+        <li><a href="https://stackoverflow.com/questions/1016080/why-are-html-character-entities-necessary">A Stack Overflow thread on the topic.</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references">A Wikipedia artcile on the topic.</a></li>
+        <li><a href="https://dev.w3.org/html5/html-author/charref">The W3C reference.</a></li>
+    </ul>
+    <h2>Summary</h2>
+    <p>In summation...</p>
+  </body>
+</html>
---
+++ resume.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello, Welcome to Chad Svastisalee's Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+<body>
+<header>
+      <span class="logo">My Website</span>
+      <a id="toggleMenu">Menu</a>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="resume.php">Resume</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+<h1>Chad Svastisalee</h1>
+<div>
+  <a href="https://www.linkedin.com/in/chadsvastisalee" target="_blank" rel="noopener">LinkedIn</a>
+  &#x25CF;
+  Chicago, IL
+</div>
+<h2>Creative Director | Project Manager</h2>
+<p>Full Stack Web and Hybrid Mobile Applications Developer
+Full stack web and hybrid mobile applications developer specializing in full stack JavaScript application
+and architectures. Experienced in all stages of the development life cycle, well versed in numerous
+programming languages.</p>
+<ul>● Hands-on experience leading all stages of system development efforts, including requirements in
+definition design, architecture, testing and support.
+● Outstanding leadership abilities; able to coordinate and direct all phases of project-based efforts.</ul>
+<h3>Core Competencies</h3>
+<ul>◦ Full Stack Development
+◦ Hybrid Mobile Development
+◦ Strong Analytical Skills
+◦ Savvy Problem Solver
+◦ Prioritizes Tasks
+◦ Strong Leadership Skills</ul>
+<h2>CERTIFICATIONS / TECHNICAL PROFICIENCIES</h2>
+<p>Certifications: Agile Certified Scrum Master
+Platforms ​ : Linux, Windows, Mac, LAMP, MEAN, NodeJS
+Database: MySQL, MongoDB
+Tools: VS Code, SSH, Gulp, Git
+Languages: HTML, CSS, SASS, JavaScript, ES6, PHP, Bash, SQL</p>
+<h2>PROFESSIONAL EXPERIENCE</h2>
+<h3>Bob’s Custom Websites – Chicago, IL<h3>
+2015 – Present
+Bob’s Custom Websites builds custom web applications for clients across a large number of industries.
+Web Developer
+<ul>●
+●
+●</ul>
+<p>Work with ES6, NodeJS, HTML, JavaScript, CSS, MySQL and MongoDB to build customized
+web applications for a diverse set of customers.
+Designed the application to meet the users’ requirements document.
+Ensured corporate and department objectives were accomplished in accordance with outlined
+objectives and mission statements.</p>
+<h3>Key Contributions:</h3>
+<ul>● Developed and implemented procedures and guidelines, optimizing productivity and efficiency;
+generating significant cost savings.
+● Recognized for the development of excellent business relationships, providing exemplary customer
+service.</ul>
+<h2>EDUCATION</h2>
+<h3>DeVry University – Chicago, IL</h3>
+<p>Bachelor of Science in Computer Information Systems
+MicroTrain Technologies – Chicago, IL</p>
+<p>Agile Full Stack Web and Hybrid Mobile Application Development
+2011 – 2015</p>
+<main>
+
+</main>
+</body>
+</html>
